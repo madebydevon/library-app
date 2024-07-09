3ee5b4cf0c702610acd3d51798d984b957b01290
Devon <174902334+madebydevon@users.noreply.github.com>
2024-07-10 01:59:26 +0200

    Display library to HTML & add delete button

--- scripts/app.js
+++ scripts/app.js
@@ -15,20 +15,7 @@ document.getElementById("create").addEventListener("click", () => {
 
 // create the library itself
 
-const myLibrary = [
-  {
-    title: "The Hobbit",
-    author: "J.R.R. Tolkien",
-    pages: 295,
-    read: "Have read",
-  },
-  {
-    title: "Book name",
-    author: "Legendary author",
-    pages: 502,
-    read: "Have read",
-  },
-];
+const myLibrary = [];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -42,21 +29,25 @@ function addBookToLibrary(title, author, pages, read) {
   let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
 
-  displayLibrary();
+  displayLibrary(myLibrary);
 }
 
 function processFormData() {
   const titleInput = document.querySelector("input[name='book-title']");
   let bookTitle = titleInput.value;
+  titleInput.value = "";
 
   const authorInput = document.querySelector("input[name='book-author']");
   let bookAuthor = authorInput.value;
+  authorInput.value = "";
 
   const pagesInput = document.querySelector("input[name='book-pages']");
   let totalPages = pagesInput.value;
+  pagesInput.value = "";
 
   const readStatusInput = document.querySelector("input[name='read-status']");
   let readStatus = translateReadStatus(readStatusInput.checked);
+  readStatusInput.checked = false;
 
   if (validateInputs(`${bookTitle}`, `${bookAuthor}`, `${totalPages}`) == true) {
     addBookToLibrary(`${bookTitle}`, `${bookAuthor}`, `${totalPages}`, `${readStatus}`);
@@ -86,6 +77,50 @@ function translateReadStatus(readStatus) {
   }
 }
 
-function displayLibrary() {
-  document.createElement;
+function displayLibrary(libraryArray) {
+  const bookContainer = document.querySelector("div.lib-wrapper");
+  clearDisplay(bookContainer);
+
+  for (let items = 0; items < libraryArray.length; items++) {
+    const wrapper = document.createElement("div");
+    const title = document.createElement("h2");
+    const author = document.createElement("p");
+    const pages = document.createElement("p");
+    const read = document.createElement("button");
+    const button = document.createElement("button");
+
+    let bookWrapper = bookContainer.appendChild(wrapper);
+    bookWrapper.classList.add("lib-item");
+    let bookTitle = bookWrapper.appendChild(title);
+    let bookAuthor = bookWrapper.appendChild(author);
+    let totalPages = bookWrapper.appendChild(pages);
+    let readStatus = bookWrapper.appendChild(read);
+    let deleteButton = bookWrapper.appendChild(button);
+
+    bookTitle.textContent = `${libraryArray[items].title}`;
+    bookAuthor.textContent = `${libraryArray[items].author}`;
+    totalPages.textContent = `${libraryArray[items].pages}`;
+    readStatus.textContent = `${libraryArray[items].read}`;
+    deleteButton.textContent = "Delete book";
+
+    deleteButton.addEventListener("click", () => {
+      libraryArray.splice(items, items + 1);
+      clearDisplay(bookWrapper);
+      displayLibrary(myLibrary);
+    });
+
+    readStatus.addEventListener("click", () => {
+      if (libraryArray[items].read == "Have read") {
+        libraryArray[items].read = "Have not read yet";
+        readStatus.textContent = `${libraryArray[items].read}`;
+      } else {
+        libraryArray[items].read = "Have read";
+        readStatus.textContent = `${libraryArray[items].read}`;
+      }
+    });
+  }
+}
+
+function clearDisplay(element) {
+  element.textContent = "";
 }
